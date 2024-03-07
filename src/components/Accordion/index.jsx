b92151c696fa718de39559db0e88d3ea563a0749
Jean Fellipe <jeanfellipesilva93@gmail.com>
2024-03-06 22:27:19 -0300

    add multi selection accordion

--- src/components/Accordion/index.jsx
+++ src/components/Accordion/index.jsx
@@ -8,6 +8,8 @@ import "./styles.css"
 export default function Accordion () {
 
 	const [selected, setSelected] = useState(null)
+	const [enableMultiSelection, setEnableMultiSelection] = useState(false)
+	const [multiple, setMultiple] = useState([])
 
 	function handleSingleSelection (itemId) {
 
@@ -18,21 +20,40 @@ export default function Accordion () {
 		}
 	}
 
+	function handleMultiSelection (itemId) {
+		let copyMultiple = [...multiple]
+
+		const findIndexOfCurrentId = copyMultiple.indexOf(itemId)
+
+		if (findIndexOfCurrentId === -1) copyMultiple.push(itemId)
+		else copyMultiple.splice(findIndexOfCurrentId, 1)
+
+		setMultiple(copyMultiple)
+
+	}
+
 	return (
 		<div className="wrapper">
+			<button className={enableMultiSelection? "pressed" : null} onClick={() => setEnableMultiSelection(!enableMultiSelection)}>Enable Multi Selection</button>
 			<div className="accordion">
 				{data && data.length > 0 ? data.map(item => (
 					<div key={item.id} className="item">
-						<div onClick={() => handleSingleSelection(item.id)} className="title">
+						<div 
+							onClick={enableMultiSelection 
+								? () => handleMultiSelection(item.id) 
+								: () => handleSingleSelection(item.id)} className="title">
 							<h3>{item.question}</h3>
 							<span>+</span>
 						</div>
-						{
-							selected === item.id ? 
+						{enableMultiSelection 
+							? multiple.indexOf(item.id) !== -1 && (
+								<div className="content">
+									{item.answer}
+								</div> )
+							: selected === item.id && 
 								<div className="content">
 									{item.answer}
 								</div> 
-								: null
 						}
 					</div>
 				)) 
